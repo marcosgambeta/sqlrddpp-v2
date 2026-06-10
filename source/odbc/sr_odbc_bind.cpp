a0479a0be2e857d335a5362f35132c8534863c1e
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2026-06-10 20:24:47 -0300

    Change '(SQLLEN *)' to 'static_cast<SQLLEN *>'

--- source/odbc/sr_odbc_bind.cpp
+++ source/odbc/sr_odbc_bind.cpp
@@ -751,7 +751,7 @@ HB_FUNC_STATIC(SR_ODBCGETLINES)
       } else {
         do {
           wResult = SQLGetData(SR_PAR_SQLHSTMT(1), static_cast<SQLUSMALLINT>(lIndex), static_cast<SQLSMALLINT>(SQL_CHAR), (PTR)bBuffer,
-                               static_cast<SQLLEN>(lLen), (SQLLEN *)&lLenOut);
+                               static_cast<SQLLEN>(lLen), static_cast<SQLLEN *>(&lLenOut));
           if (wResult == SQL_SUCCESS && iReallocs == 0) {
             sr_odbcFieldGet(hb_arrayGetItemPtr(pFields, i), &temp, (char *)bBuffer, lLenOut, 0, ulSystemID, bTranslate);
             hb_arraySetForward(pLine, i, &temp);
@@ -928,7 +928,7 @@ HB_FUNC_STATIC(SR_COLATTRIBUTE)
   SQLLEN wNumPtr = static_cast<SQLLEN>(hb_parnint(7));
   RETCODE wResult =
       SQLColAttribute(SR_PAR_SQLHSTMT(1), SR_PAR_SQLUSMALLINT(2), SR_PAR_SQLUSMALLINT(3), static_cast<SQLPOINTER>(bBuffer),
-                      SR_PAR_SQLSMALLINT(5), static_cast<SQLSMALLINT *>(&wBufLen), (SQLLEN *)&wNumPtr);
+                      SR_PAR_SQLSMALLINT(5), static_cast<SQLSMALLINT *>(&wBufLen), static_cast<SQLLEN *>(&wNumPtr));
 
   if (wResult == SQL_SUCCESS || wResult == SQL_SUCCESS_WITH_INFO) {
     // hb_storclen((LPSTR) bBuffer, static_cast<int>(wBufLen), 4);
